c7fae8c5d94688563a933b1ae99c0e09863f39f1
anthony <anthony1320081@hotmail.com>
2024-03-24 20:09:34 -0500

    nuevos modelos agregados a pos config y pos res config

--- modulo_uom_pos/models/pos_config.py
+++ modulo_uom_pos/models/pos_config.py
@@ -4,5 +4,27 @@ from odoo import models, fields, _
 class PosConfig(models.Model):
       
       _inherit = 'pos.config'
-  
-      
+      product_multi_uom = fields.Boolean(string='Habilitar multiples medidas', default=False)
+
+class PosOrder(models.Model):
+    
+    _inherit = 'pos.order'
+    uom = fields.Boolean(string='Uom', related='config_id.product_multi_uom', readonly=True)
+
+class PosOrderLine(models.Model):
+    _inherit = 'pos.order.line'
+    uom_id = fields.Many2one('uom.uom', string="Unidad de medida")
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    product_uom_ids = fields.One2many('product.template.uom.line', 'product_uom_line_id', string="Lineas descarte")
+    point_of_sale_uom = fields.Boolean(string="UOM punto de venta")
+
+class ProductTemplateUomLine(models.Model):
+    _name = 'product.template.uom.line'
+    _description = 'UOM Line'
+
+    product_uom_line_id = fields.Many2one('product.template', string="Product UOM")
+    unit_of_measure_id = fields.Many2one('uom.uom', string="unidad de medida")
+    sale_price = fields.Float(string="precio de venta")
---
+++ modulo_uom_pos/models/pos_res_config.py
@@ -0,0 +1,7 @@
+from odoo import fields, models, api, _
+
+
+class ResConfigSettings(models.TransientModel):
+	_inherit = 'res.config.settings'
+
+	pos_product_multi_uom = fields.Boolean(related='pos_config_id.product_multi_uom', readonly=False)
