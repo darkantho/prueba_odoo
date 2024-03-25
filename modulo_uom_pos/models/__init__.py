d6a42bd8561a47352ff15d83585ff68282551433
anthony <anthony1320081@hotmail.com>
2024-03-24 19:56:33 -0500

    nuevos archivos en models

--- modulo_uom_pos/models/__init__.py
+++ modulo_uom_pos/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import pos_config
+from . import pos_res_config
---
+++ modulo_uom_pos/models/pos_config.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, _
+
+
+class PosConfig(models.Model):
+      
+      _inherit = 'pos.config'
+  
+      
